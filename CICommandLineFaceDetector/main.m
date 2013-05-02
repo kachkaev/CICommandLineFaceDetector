4e8e4025c4f9dba0ca5cb35b6dc9e9c320bbd303
Alexander Kachkaev <alexander@kachkaev.ru>
2013-05-02 11:47:45 +0100

    Initial Commit

---
+++ CICommandLineFaceDetector/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CICommandLineFaceDetector
+//
+//  Created by Alexander Kachkaev on 02/05/2013.
+//  Copyright (c) 2013 Alexander Kachkaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
